29cfacea4a7acb0af0f6ae3a6f48dfaa791f72ff
arrrrrmin <43553423+arrrrrmin@users.noreply.github.com>
2025-08-17 14:16:10 +0200

    🔨: Safety commit

--- wiki-history-viewer/src/lib/components/Rideline.svelte
+++ wiki-history-viewer/src/lib/components/Rideline.svelte
@@ -3,44 +3,67 @@
     import { onMount } from "svelte";
     import * as d3 from "d3";
 
-    import * as utils from "./utils";
+    import { IdHandler } from "$lib/components/utils";
 
-    let { id: string, revisions = $bindable(), ...props } = $props();
+    let {
+        revisions = $bindable(),
+        dataOptions = $bindable(),
+        ...props
+    } = $props();
 
+    // Visualisation variables
     const id = props.id;
+    const idHandler = new IdHandler(id);
     const width = 1000;
     const height = 900;
     const margins = { right: 20, top: 20, left: 80, bottom: 40 };
-    const overlap = 5;
+    const overlap = 8;
     const t = 300;
 
+    // Internal helper variables
+    let revs = [];
     let timestamps = [];
     let deltas = [];
     let series = [];
 
-    const getElements = (elementName) => {
-        return revisions.map((r) => r[elementName]);
-    };
-
-    const getEditors = () => {
-        return d3.groups((r) => r.user.name, revisions);
-    };
-
-    let x = undefined;
-    let y = undefined;
-    let z = undefined;
+    // Variables used inside the svg
+    let x = d3.scaleTime().range([width - margins.right, margins.left]);
+    let y = d3.scalePoint().range([margins.top, height - margins.bottom]);
+    let z = d3.scaleLinear().nice();
     let svg = undefined;
+    let titleEl = undefined;
     let groups = undefined;
 
+    // Todo move this to a utils file
+    const transformInputData = ({
+        allowMinors = true,
+        allowUnknownEditors = true,
+        filterMinValue = 0,
+    }) => {
+        // Transform input data for internal usage
+        if (!revisions) return;
+        revs = revisions.slice();
+
+        revs = revs.filter((r) => r.size);
+        if (!allowMinors) {
+            revs = revs.filter((r) => !r.minor);
+        }
+        if (!allowUnknownEditors) {
+            revs = revs.filter((r) => !r.user.name.startsWith("~"));
+        }
+    };
+
     const getTimestamps = () => {
-        return Array.from(d3.group(revisions, (d) => +d.timestamp).keys()).sort(
+        // Provide all unqiue timestamps sorted ascending
+        return Array.from(d3.group(revs, (d) => +d.timestamp).keys()).sort(
             d3.ascending,
         );
     };
 
     const getSeries = () => {
+        // Provide the series data such that every editor has a revisions value per timestamp
         return d3
-            .groups(revisions, (d) => d.user.name)
+            .groups(revs, (d) => d.user.name)
             .map(([name, values]) => {
                 const value = new Map(
                     values.map((d) => [+d.timestamp, Math.abs(d.size)]),
@@ -50,9 +73,10 @@
                     values: timestamps.map((d) => value.get(d) | 0),
                 };
             })
-            .sort((a, b) => d3.sum(a.values) - d3.sum(b.values));
+            .sort((a, b) => d3.sum(b.values) - d3.sum(a.values));
     };
 
+    // Generators for area and line
     let area = d3
         .area()
         .curve(d3.curveBasis)
@@ -63,35 +87,38 @@
 
     let line = area.lineY1();
 
+    // Initially called function that creates diagram basics
     onMount(() => {
+        transformInputData(dataOptions);
         timestamps = getTimestamps();
         series = getSeries();
 
-        // Create the scales.
-        x = d3.scaleTime().range([width - margins.right, margins.left]);
-        y = d3.scalePoint().range([margins.top, height - margins.bottom]);
-        z = d3.scaleLinear().nice();
-
         x.domain(d3.extent(timestamps));
         y.domain(series.map((d) => d.name));
-        z.domain([0, d3.max(series, (d) => d3.max(d.values))]).range([
+        z.domain([0, d3.max(series, (d) => d3.median(d.values))]).range([
             0,
-            -overlap * y.step(),
+            -40, // -overlap * y.step(),
         ]);
 
-        // Create the SVG container.
         svg = d3
             .select(`div#${id}`)
             .append("svg")
+            .attr("class", "m-auto")
             .attr("width", width)
-            .attr("height", height)
-            .attr("viewBox", [0, 0, width, height])
+            //.attr("height", height)
             .attr("style", "max-width: 100%; height: auto; overflow: visible;");
 
-        // Append the axes.
-        var gx = svg
-            .append("g")
-            .attr("id", "gx")
+        svg.transition(t)
+            .ease(d3.easeExpInOut)
+            .attr("height", height)
+            .attr("viewBox", [0, 0, width, height]);
+
+        d3.select(`h3#${id}`).html(
+            `Edits regarding the article '${props.result.title.replaceAll("_", " ")}' in the ${props.result.lang}-Wikipedia`,
+        );
+
+        svg.append("g")
+            .attr("id", idHandler.register("g", "gx"))
             .attr("transform", `translate(0,${height - margins.bottom})`)
             .call(
                 d3
@@ -101,17 +128,19 @@
             );
 
         svg.append("g")
-            .attr("id", "gy")
+            .attr("id", idHandler.register("g", "gy"))
             .attr("transform", `translate(${margins.left},0)`)
             .call(d3.axisLeft(y).tickSize(0).tickPadding(4))
             .call((g) => g.select(".domain").remove());
 
-        groups = svg.append("g").attr("id", "groups");
+        groups = svg.append("g").attr("id", idHandler.register("g", "groups"));
 
         update();
     });
 
     const update = () => {
+        // Update function to fill the diagram with the actual data and handle transitions, updates and exits
+        transformInputData(dataOptions);
         timestamps = getTimestamps();
         series = getSeries();
 
@@ -119,10 +148,10 @@
         y.domain(series.map((d) => d.name));
         z.domain([0, d3.max(series, (d) => d3.max(d.values))]).range([
             0,
-            -overlap * y.step(),
+            -40, // -overlap * y.step(),
         ]);
 
-        d3.select("g#gx")
+        d3.select(idHandler.selector("g", "gx"))
             .call(
                 d3
                     .axisBottom(x)
@@ -147,64 +176,56 @@
                     .text("↑ Latest Edit");
             });
 
-        d3.select("g#gy")
+        d3.select(idHandler.selector("g", "gy"))
             .call(d3.axisLeft(y).tickSize(0).tickPadding(4))
             .call((g) => g.select(".domain").remove());
 
-        d3.select("g#groups")
-            .selectAll("g#series-container")
+        d3.select(idHandler.selector("g", "groups"))
+            .selectAll(idHandler.selector("g", "series-container"))
             .data(series)
             .join(
                 (enter) => {
                     var container = enter
                         .append("g")
-                        .attr("id", "series-container")
-                        .call((enter) =>
-                            enter.attr(
-                                "transform",
-                                (d) => `translate(0,${y(d.name) + 1})`,
-                            ),
+                        .attr("id", idHandler.register("g", "series-container"))
+                        .attr(
+                            "transform",
+                            (d) => `translate(0,${y(d.name) + 1})`,
                         );
                     container
                         .append("path")
-                        .attr("id", "area")
+                        .attr("id", idHandler.register("path", "area"))
                         .attr("fill", "#ddd")
                         .attr("d", (d) => area(d.values));
                     container
                         .append("path")
-                        .attr("id", "line")
+                        .attr("id", idHandler.register("path", "line"))
                         .attr("fill", "none")
                         .attr("stroke", "black")
                         .attr("d", (d) => line(d.values));
                 },
                 (update) => {
-                    var container = update.call((update) =>
-                        update
-                            .transition(t)
-                            .ease(d3.easeExpIn)
-                            .attr(
-                                "transform",
-                                (d) => `translate(0,${y(d.name) + 1})`,
-                            ),
-                    );
+                    var container = update
+                        .transition(t)
+                        .ease(d3.easeExpInOut)
+                        .attr(
+                            "transform",
+                            (d) => `translate(0,${y(d.name) + 1})`,
+                        );
 
                     container
-                        .select("path#area")
+                        .select(idHandler.selector("path", "area"))
                         .attr("d", (d) => area(Array(d.values.length).fill(0)))
-                        // .call((update) => update.transition(t))
-                        //.transition(t)
-                        //.ease(d3.easeExpIn)
                         .attr("d", (d) => area(d.values));
 
                     container
-                        .select("path#line")
+                        .select(idHandler.selector("path", "line"))
                         .attr("d", (d) => area(Array(d.values.length).fill(0)))
-                        // .call((update) => update.transition(t))
-                        //.transition(t)
-                        //.ease(d3.easeExpIn)
                         .attr("d", (d) => line(d.values));
                 },
-                (exit) => exit,
+                (exit) => {
+                    exit.remove();
+                },
             );
     };
 
@@ -216,4 +237,10 @@
     });
 </script>
 
-<div {id} style="margin-top: 100px;"></div>
+<!-- Title container -->
+<div class="m-auto max-w-4xl py-4 px-4 sm:px-6 lg:px-8">
+    <!-- svelte-ignore a11y_missing_content -->
+    <h3 {id} class="text-xl font-semibold text-gray-900"></h3>
+</div>
+<!-- Container where the visualisation is rendered in. -->
+<div {id} class="pt-2"></div>
